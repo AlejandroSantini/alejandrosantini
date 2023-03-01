aef4ccfcd34e5ff84b134f3b4f6d166262924496
AlejandroSantini <alejandro.j.santini@gmail.com>
2023-03-01 19:31:31 -0300

    se creo rama nueva

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,8 @@ import img from '../../assets/images/developer.png';
 import styles from './about.module.css';
 
 const AboutMe = () => {
+  const constantedeprueba = 1
+  console.log(constantedeprueba)
   return (
     <section id="aboutMe" className={styles.container}>
       <img src={img} alt="Developer image" />
