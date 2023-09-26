d2ffa759b9d7e33dfa8875bbdf75f4c00f5aa167
Alejandro Santini <alejandro.j.santini@gmail.com>
2023-09-26 17:43:42 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/layout';
+import Layout from './components/Layout';
 
 function App() {
   return (
